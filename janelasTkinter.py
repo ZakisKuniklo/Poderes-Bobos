1e45901323a218d036fc57254f70b61050bad3df
João Pedro Coelho de Sousa <joaopedro.coelhosousa1@gmail.com>
2025-09-17 11:54:03 -0300

    Ajustadas as janelas
    Ajustado posição dos elementos visuais

--- janelasTkinter.py
+++ janelasTkinter.py
@@ -21,6 +21,7 @@ def listaPoderesTkinter():
     #Instanciar Tela
     global listaPoderes
     listaPoderes = Toplevel()
+    listaPoderes.minsize(250,100)
     menu.withdraw()
     #Título
     titulo = Label (listaPoderes,text="Lista de poderes").grid(column=1,row=0,padx=5,pady=5)
@@ -29,7 +30,12 @@ def listaPoderesTkinter():
     listboxPoderes.grid(column=1,row=1,padx=5,pady=5)
     #Botões
     comentar = Button(listaPoderes,text="Comentários", command=selectItem).grid(column=1,row=3,padx=5)
-    sair = Button(listaPoderes,text="Voltar", command=fechar).grid(column=2,row=3,pady=5)
+    sair = Button(listaPoderes,text="Voltar", command=fechar).grid(column=1,row=4,pady=5)
+    #Espaçadores
+    um = Label(listaPoderes,text= ' ').grid(column=0,row=0,padx=25)
+    dois = Label(listaPoderes,text= ' ').grid(column=0,row=1,padx=25)
+    tres = Label(listaPoderes,text= ' ').grid(column=0,row=2,padx=25)
+    quatro = Label(listaPoderes,text= ' ').grid(column=0,row=3,padx=25)
     #listaPoderes.mainloop()
 
 def telaListarComentario(poder):
@@ -59,14 +65,17 @@ def telaListarComentario(poder):
     bigLista.insert(END,bigString)
     #Botões
     comentar = Button(listarComentario,text="Comentar", command=acessarComentario).grid(column=1,row=3,pady=5)
-    sair = Button(listarComentario,text="Voltar", command=fechar).grid(column=2,row=3,pady=5)
+    sair = Button(listarComentario,text="Voltar", command=fechar).grid(column=1,row=4,pady=5)
 
 def telaAdicionarComentario(poder):
     #Funções
     def fechar():
         fecharJanela(menu,adicionarComentario)
     def addCommentario():
-        cm.addComment(entryNick.get(),poder,textComment.get("1.0",END).rstrip())
+        nick = entryNick.get()
+        comment = textComment.get("1.0",END).rstrip()
+        if (nick != "" and comment!= ""):
+            cm.addComment(nick,poder,comment)
         textComment.delete("1.0",END)
         entryNick.delete(0,END)
     #Instanciar tela
@@ -94,17 +103,21 @@ def telaGanharPoder():
     ganharPoder = Toplevel()
     ganharPoder.title("Você ganhou um poder")
     ganharPoder.minsize(250,100)
+    ganharPoder.resizable(0,0)
     #ganharPoder.overrideredirect(True)
     menu.withdraw()
     poder = pa.pegaPoder()
     print(poder)
     titulo = Label(ganharPoder,text=str(poder['nome']).encode('utf-8'))
-    titulo.grid(column=0,row=0,pady=5)
+    titulo.grid(column=1,row=0,pady=5)
     texto = Text(ganharPoder,height=10,width=20)
-    texto.grid(column=0,row=1,pady=5)
+    texto.grid(column=1,row=1,pady=5)
     texto.insert(INSERT,str(poder['desc']).encode('utf-8'))
-    sair = Button(ganharPoder,text="Voltar", command=fechar).grid(column=2,row=1,pady=5)
-
+    sair = Button(ganharPoder,text="Voltar", command=fechar).grid(column=1,row=2,pady=5)
+    #Espaçadores
+    um = Label(ganharPoder,text= ' ').grid(column=0,row=0,padx=15)
+    dois = Label(ganharPoder,text= ' ').grid(column=0,row=1,padx=15)
+    tres = Label(ganharPoder,text= ' ').grid(column=0,row=2,padx=15)
 
 def telaAdicionarPoder():
     #Funções
@@ -157,10 +170,16 @@ def MenuTkinter():
     #Título
     titulo = Label(menu,text= 'Poderes bobos!!!').grid(column=1,row=0,pady=5)
     #Botões
-    listarPoderes = Button(menu,text="Listar Poderes",command=listaPoderesTkinter).grid(column=1,row=1,padx=5,pady=5)
-    ganharPoder = Button(menu,text="Ganhar Poder", command=telaGanharPoder).grid(column=1,row=2,padx=5,pady=5)
-    adicionarPoder = Button(menu,text="Adicionar Poder",command=telaAdicionarPoder).grid(column=1,row=3,padx=10,pady=5)
+    listarPoderes = Button(menu,text="Listar Poderes",command=listaPoderesTkinter).grid(column=1,row=1,pady=5)
+    ganharPoder = Button(menu,text="Ganhar Poder", command=telaGanharPoder).grid(column=1,row=2,pady=5)
+    adicionarPoder = Button(menu,text="Adicionar Poder",command=telaAdicionarPoder).grid(column=1,row=3,pady=5)
     sair = Button(menu,text="Sair", command=menu.destroy).grid(column=1,row=4,pady=5)
+    #Espaçadores
+    um = Label(menu,text= ' ').grid(column=0,row=0,padx=32)
+    dois = Label(menu,text= ' ').grid(column=0,row=1,padx=32)
+    tres = Label(menu,text= ' ').grid(column=0,row=2,padx=32)
+    quatro = Label(menu,text= ' ').grid(column=0,row=3,padx=32)
+    cinco = Label(menu,text= ' ').grid(column=0,row=4,padx=32)
     #Manter janela Aberta
     menu.mainloop()
 
